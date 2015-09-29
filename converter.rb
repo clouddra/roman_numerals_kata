8e9251deaa16e36a2cdff33dec8fa6a911524bb6
Yun Long <chong.yunlong@gmail.com>
2015-09-29 13:06:27 +0800

    implemented specs/functionality for decimal_to_roman

--- converter.rb
+++ converter.rb
@@ -1,7 +1,29 @@
 class Converter
+  DECIMAL_ROMAN = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
+
   def decimal_to_roman(decimal)
-    raise "NotImplementedError"
+    n, result = decimal, ''
+    DECIMAL_ROMAN.each_pair do |k, v|
+      q, n = n.divmod(k)
+      result << v * q
+    end
+    result
   end
+
 
   def roman_to_decimal(roman)
     raise "NotImplementedError"
